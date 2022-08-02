05848c8deb3649405d1377bc300f0fdcae524be4
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2022-08-02 09:32:36 +0530

    done with major part of project

--- Text_to_Speech/app.js
+++ Text_to_Speech/app.js
@@ -6,7 +6,7 @@ const textInput = document.querySelector('#text-input');
 const voiceSelect = document.querySelector('#voice-select');
 const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#rate-value');
-const pitch = document.querySelector('pitch');
+const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
 
 
@@ -43,3 +43,66 @@ if (synth.onvoiceschanged !== undefined) {
 }
 
 //Speak function initialisation
+const speak=()=>{
+    //Check if speaking
+    if(synth.speaking){
+        console.error('Already speaking...');
+        return;
+    }
+
+    if(textInput.value !== ''){
+        //Get textbox text
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+        //speak end
+        speakText.onend = e=>{
+            console.log("Done Speaking");
+        }
+
+        //Speak error
+
+        speakText.error = e=>{
+            console.error("Something went wrong");
+        }
+
+        //Selected voice 
+        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+        //Loop through voices
+
+        voices.forEach(voice=>{
+            if(voice.name===selectedVoice){
+                speakText.voice = voice;
+            }
+        });
+
+        //Setting pitch and rate
+        speakText.rate=rate.value;
+        speakText.pitch=pitch.value;
+
+        //Speak
+        synth.speak(speakText);
+
+    }
+};
+
+//Setting event listeners
+
+//text form submit
+
+textForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+//rate value change
+
+rate.addEventListener('change',e=>rateValue.textContent=rate.value);
+
+//pitch value change
+
+pitch.addEventListener('change',e=>pitchValue.textContent=pitch.value);
+
+//Voice select change
+voiceSelect.addEventListener('change',e=>speak());
